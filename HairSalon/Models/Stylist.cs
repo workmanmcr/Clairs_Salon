258c111af304be3567b962a42446f2835c77cb80
workmanmcr <workmanmcr@gmail.com>
2023-07-28 14:13:45 -0700

    add more cshtml

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,53 +1,17 @@
 using System.Collections.Generic;
-using MySqlConnector;
 
 namespace HairSalon.Models
 {
-    public class Stylist
+  public class Stylist
+  {
+    public Stylist()
     {
-        public Stylist()
-        {
-            this.Clients = new HashSet<Client>();
-        }
-
-        public int StylistId { get; set; }
-        public string Name { get; set; }
-        public string Number { get; set; }
-        public virtual ICollection<Client> Clients { get; set; }
-
-        // Update the GetAll method to fetch data from the database
-        public static List<Stylist> GetAll()
-        {
-            List<Stylist> allStylists = new List<Stylist>();
-
-            using (MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString))
-            {
-                conn.Open();
-
-                string query = "SELECT * FROM stylists;";
-
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
-                using (MySqlDataReader rdr = cmd.ExecuteReader())
-                {
-                    while (rdr.Read())
-                    {
-                        int stylistId = rdr.GetInt32(0);
-                        string name = rdr.GetString(1);
-                        string number = rdr.GetString(2);
-
-                        Stylist newStylist = new Stylist
-                        {
-                            StylistId = stylistId,
-                            Name = name,
-                            Number = number
-                        };
-
-                        allStylists.Add(newStylist);
-                    }
-                }
-            }
-
-            return allStylists;
-        }
+      this.Clients = new HashSet<Client>();
     }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Number { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
 }
--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using HairSalon.Models;
+using Models;
 
 namespace HairSalon
 {
