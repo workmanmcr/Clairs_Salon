fe6519c8f9eff9584cf9e8f5f87c7686d38c0532
workmanmcr <workmanmcr@gmail.com>
2023-07-28 14:41:25 -0700

    add functions to stylist model

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,13 +1,38 @@
+using System.Collections.Generic;
 
 namespace HairSalon.Models
 {
-  public class Client
-  {
-    public int ClientId { get; set; }
-    public string Name { get; set; }
-    public string Number { get; set; }
-    public int StylistId { get; set; }
-    
-    public virtual Stylist Stylist { get; set; }
-  }
+    public class Client
+    {
+      
+       
+        
+       
+        public string ClientName { get; set; }
+        public int Number { get; set; }
+        public int Id { get; }
+        private static List<Client> _instances = new List<Client> { };
+        private static int _idCounter = 1;
+
+        public Client( string clientName, int number, int id)
+        {
+           
+            ClientName = clientName;
+            Number = number;
+            Id = _idCounter;
+            _idCounter++;
+            _instances.Add(this);
+        }
+
+        public static List<Client> GetAll()
+        {
+            return _instances;
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+            _idCounter = 1; 
+        }
+    }
 }
