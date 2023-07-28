e6bdec6f1dac4116bb01eeb0f524950fe11ab200
workmanmcr <workmanmcr@gmail.com>
2023-07-28 10:58:05 -0700

    add controllers

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,10 +5,7 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
-    public string Address { get; set; }
     public string Number { get; set; }
-    public string Email { get; set; }
-    public string ReferredBy { get; set; }
     public int StylistId { get; set; }
     
     public virtual Stylist Stylist { get; set; }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -12,7 +12,6 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public string Number { get; set; }
-    public string Email { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
   }
 }
