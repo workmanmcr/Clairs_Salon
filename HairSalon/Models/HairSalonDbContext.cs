8e4b880f1c3e0e943a8fb0c37a41774253acce89
workmanmcr <workmanmcr@gmail.com>
2023-07-29 10:15:09 -0700

    look for MySql bug fixes

--- HairSalon/Models/HairSalonDbContext.cs
+++ HairSalon/Models/HairSalonDbContext.cs
@@ -2,10 +2,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Models
 {
-    public class HairSalonContext : DbContext
+    public class HairSalonDbContext : DbContext
     {
         public virtual DbSet<Stylist> Stylists { get; set; }
         public DbSet<Client>  Clients { get; set; }
-        public HairSalonContext(DbContextOptions options) : base(options) { }
+        public HairSalonDbContext(DbContextOptions options) : base(options) { }
     }
 }
